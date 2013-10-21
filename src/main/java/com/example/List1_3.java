ec572d4be4b8e1bdc3301deeeb5244f8ec4c6e36
yuya <yuya@fuji-PC>
2013-10-21 22:38:35 +0900

    add List1_3.java

---
+++ src/main/java/com/example/List1_3.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class List1_3 {
+
+	public static void main(String[] args) {
+		String s1 = "Java Programing";
+		System.out.println("文字列s1の3文字目以降は" + s1.substring(3));	// a Programing
+		System.out.println("文字列s1の3~8文字列は" + s1.substring(3,9));	// a Prog
+	}
+
+}
