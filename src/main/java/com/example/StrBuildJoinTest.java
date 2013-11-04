34957f7e0e210c7220328895bc67d8ea9cc915df
yuya <yuya@fuji-PC>
2013-11-04 22:22:50 +0900

    add list8_1~2

--- src/main/java/com/example/StrBuildJoinTest.java
+++ src/main/java/com/example/StrBuildJoinTest.java
@@ -4,13 +4,14 @@ public class StrBuildJoinTest {
 
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		StringBuilder strbr = new StringBuilder();
-		for(int i = 0; i < 100000; i++) {
+		StringBuilder strbr = new StringBuilder("test\n");
+		for(int i = 0; i < 1000000; i++) {
 			strbr.append("test\n");
 		}
 		long stop = System.currentTimeMillis();
+		
 		System.out.println(stop - start);
-		System.out.println(strbr);
+		
 	}
 
 }
--- src/main/java/com/example/StrJoinTest.java
+++ src/main/java/com/example/StrJoinTest.java
@@ -5,7 +5,7 @@ public class StrJoinTest {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		String str = "test\n";
-		for(int i = 0; i < 10000; i++) {
+		for(int i = 0; i < 100000; i++) {
 			str += "test\n";
 		}
 		long stop = System.currentTimeMillis();
